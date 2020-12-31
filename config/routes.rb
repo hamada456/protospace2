727c943d2450264bb81dfa81aedd0d1c7bf104d0
hamada456 <n87.takaya.hamada@gmail.com>
2020-12-31 17:16:37 +0900

    名前変更

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 Rails.application.routes.draw do
   devise_for :users
-  root to: "protospace#index"
+  root to: "prototype#index"
 end
--- test/controllers/prototype_controller_test.rb
+++ test/controllers/prototype_controller_test.rb
@@ -1,6 +1,6 @@
 require 'test_helper'
 
-class ProtospaceControllerTest < ActionDispatch::IntegrationTest
+class PrototypeControllerTest < ActionDispatch::IntegrationTest
   # test "the truth" do
   #   assert true
   # end
