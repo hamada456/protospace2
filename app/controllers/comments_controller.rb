42924fea15e146781c8ae89e9b17c46543967151
hamada456 <n87.takaya.hamada@gmail.com>
2021-02-19 19:06:26 +0900

    ストパラ、idのmerge（２つ）

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,4 +1,11 @@
 class CommentsController < ApplicationController
+
   def create
   end
+
+  private
+  def comment_params
+    params.require(:comment).merge(user_id: current_user.id, prototype_id:)
+  end
+
 end
